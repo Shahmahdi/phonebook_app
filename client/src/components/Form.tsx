3f90212151a10865c3914bcfea019da0af393499
mahdi <shahmahdi11@gmail.com>
2020-02-07 00:27:41 +0600

    code refactor

--- client/src/components/Form.tsx
+++ client/src/components/Form.tsx
@@ -18,7 +18,7 @@ const ContactFormComponent = (props: {
       _id: props.contactInfo!._id,
       ...values
     })
-    : await props.createContact(values);
+      : await props.createContact(values);
 
     if (response.status === 200 || response.status === 201) {
       Toaster.create().show({
@@ -39,80 +39,80 @@ const ContactFormComponent = (props: {
             name: props.contactInfo ? props.contactInfo.name : '',
             phoneNumber: props.contactInfo ? props.contactInfo.phoneNumber : ''
           }}
-          render={({ handleSubmit, submitting, pristine, hasSubmitErrors, hasValidationErrors,valid }) => (
-            <>
-              <form
-                className="dt fl cf"
-                onSubmit={handleSubmit}
-              >
-                <div className="mw9 center ph3-ns v-mid dtc">
-                  <div className="flex pt3 justify-content">
-                    <div className="fl w-100">
-                      <Field<string>
-                        name="name"
-                        validate={composeValidators(required)}
-                        render={({ input, meta }) =>
-                          <div className={` ${checkError(meta) ? 'mb3' : ''}`}>
-                            <FormGroup
-                              label="Name"
-                              labelFor="name"
-                              labelInfo="(required)"
-                            >
-                              <InputGroup
-                                id="name"
-                                placeholder="e.g. John"
-                                value={input.value}
-                                onChange={input.onChange}
-                              />
-                              {checkError(meta) &&
-                                <small className="fl w-100" style={{ color: 'red' }}>
-                                  {meta.error || meta.submitError}
-                                </small>
-                              }
-                            </FormGroup>
-                          </div>
-                        }
-                      />
-                    </div>
-                  </div>
-
-                  <div className={`flex justify-content`}>
-
-                    <div className="flex-auto">
-                      <div className="w-100">
-                        <Field<string>
-                          name="phoneNumber"
-                          validate={composeValidators(required, phoneNoValidation)}
-                          render={({ input, meta }) =>
-                            <div className={` ${checkError(meta) ? 'mb3' : ''}`}>
-                              <FormGroup
-                                label="Phone"
-                                labelFor="phone"
-                                labelInfo="(required)"
-                              >
-                                <span className="f5 fl w-20 fw5 pr2 pt1">+88</span>
-                                <InputGroup
-                                  id="phone"
-                                  className="fl w-80"
-                                  placeholder="e.g. 01***********"
-                                  value={input.value}
-                                  onChange={input.onChange}
-                                />
-                                {checkError(meta) &&
-                                  <small className="fl w-100" style={{ color: 'red' }}>
-                                    {meta.error || meta.submitError}
-                                  </small>
-                                }
-                              </FormGroup>
-                            </div>
-                          }
+          render={({ handleSubmit, submitting, pristine, hasSubmitErrors, hasValidationErrors, valid }) => (
+            <form
+              className="cf"
+              onSubmit={handleSubmit}
+            >
+              <div className="fl w-100">
+                <Field<string>
+                  name="name"
+                  validate={composeValidators(required)}
+                  render={({ input, meta }) =>
+                    <div className={` ${checkError(meta) ? 'mb3' : ''}`}>
+                      <FormGroup
+                        label="Name"
+                        labelFor="name"
+                        labelInfo="(required)"
+                      >
+                        <InputGroup
+                          id="name"
+                          placeholder="e.g. John"
+                          value={input.value}
+                          onChange={input.onChange}
                         />
-                      </div>
+                        {checkError(meta) &&
+                          <small className="fl w-100" style={{ color: 'red' }}>
+                            {meta.error || meta.submitError}
+                          </small>
+                        }
+                      </FormGroup>
                     </div>
-                  </div>
+                  }
+                />
+              </div>
+
+              <div className={`fl w-100`}>
+                <Field<string>
+                  name="phoneNumber"
+                  validate={composeValidators(required, phoneNoValidation)}
+                  render={({ input, meta }) =>
+                    <div className={` ${checkError(meta) ? 'mb3' : ''}`}>
+                      <FormGroup
+                        label="Phone"
+                        labelFor="phone"
+                        labelInfo="(required)"
+                      >
+                        <span className="f5 fl w-20 fw5 pr2 pt1">+88</span>
+                        <InputGroup
+                          id="phone"
+                          className="fl w-80"
+                          placeholder="e.g. 01***********"
+                          value={input.value}
+                          onChange={input.onChange}
+                        />
+                        {checkError(meta) &&
+                          <small className="fl w-100" style={{ color: 'red' }}>
+                            {meta.error || meta.submitError}
+                          </small>
+                        }
+                      </FormGroup>
+                    </div>
+                  }
+                />
+              </div>
+              <div className="fl w-100">
+                <div className="fl w-50 pr1">
+                  <Button
+                    text="Close"
+                    className={`bp3-popover-dismiss`}
+                    intent={Intent.DANGER}
+                    fill={true}
+                  />
+                </div>
+                <div className="fl w-50">
                   <Button
                     text="Submit"
-                    className={`${(hasValidationErrors || hasSubmitErrors) ? '' : 'bp3-popover-dismiss'}`}
                     loading={submitting}
                     type="submit"
                     disabled={submitting || pristine}
@@ -120,8 +120,8 @@ const ContactFormComponent = (props: {
                     fill={true}
                   />
                 </div>
-              </form>
-            </>
+              </div>
+            </form>
           )
           }
         />
